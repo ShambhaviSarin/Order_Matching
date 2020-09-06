c8674865959cd3a41f824efd506c279cb8a3dcbc
Shambhavi <sarin.shambhavi@gmail.com>
2020-09-07 01:23:57 +0530

    Adding express and middlewares

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(5000, ()=> {
+  console.log("server has started on port 5000")
+});
