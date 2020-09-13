a5c45a4ad4e4fd2f1d568167376948f0d454e334
RICHA0801 <37595246+RICHA0801@users.noreply.github.com>
2020-09-13 23:12:48 +0530

    #FrontPage

--- frontend/src/FrontPage.js
+++ frontend/src/FrontPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import './FrontPage.css';
 import { Link} from 'react-router-dom';
-import {Navbar,NavbarBrand,Nav,NavItem,NavLink,NavbarText,Button} from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink, NavbarText, Button } from 'reactstrap';
+import { UndrawReport } from 'react-undraw-illustrations';
 
 const NavigationBar = () => {
   return(
-    <Navbar color="link" light expand="md" style={{marginTop:'1%'}}>
+    <Navbar color="#7c7cdd" light expand="md" style={{marginTop:'1%'}}>
       <NavbarBrand href="/"><img src={require('./logo.png')} alt="logo" height="50"/></NavbarBrand>
         <Nav className="mr-auto" navbar>
           <NavItem>
@@ -21,18 +23,84 @@ const NavigationBar = () => {
   );
 }
 
+
+const Footer = () => {
+    return (
+        <div className="main-footer" style={{ background:"#7c7cdd"}}>
+        <div className="footer-middle">
+        <div className="container">
+            <div className="row">
+            <div className="col-md-3 col-sm-6">
+                <h4 style={{ marginTop: '20%' }}>header</h4>
+                <ul className="list-unstyled">
+                    <li>hello</li>
+                    <li>heya</li>
+                    <li>hola</li>
+                    <li>bonjour</li>
+                </ul>
+            </div>
+            <div className="col-md-3 col-sm-6">
+                <h4 style={{ marginTop: '20%' }}>header</h4>
+                <ul className="list-unstyled">
+                    <li>hello</li>
+                    <li>heya</li>
+                    <li>hola</li>
+                    <li>bonjour</li>
+                </ul>
+            </div>
+            <div className="col-md-3 col-sm-6">
+                <h4 style={{ marginTop: '20%' }}>header</h4>
+                <ul className="list-unstyled">
+                    <li>hello</li>
+                    <li>heya</li>
+                    <li>hola</li>
+                    <li>bonjour</li>
+                </ul>
+            </div>
+            <div className="col-md-3 col-sm-6">
+            <h4 style={{ marginTop: '20%' }}>header</h4>
+                <ul className="list-unstyled">
+                    <li>hello</li>
+                    <li>heya</li>
+                    <li>hola</li>
+                    <li>bonjour</li>
+                </ul>
+            </div>
+          </div>
+        {/*footer bottom*/}
+           <div className="footer-bottom">
+           <p className="text-xs-centre">
+            &copy;{new Date().getFullYear()} M.A.R.S.S. App - All Rights Reserved
+           </p>
+           </div>
+        </div>
+        </div>
+        </div>
+    );
+}
+
+
 const FrontPage = () => {
     return(
        <div>
-        <div style={{marginLeft: '10%', marginRight:'10%'}}>
+        <div style={{marginLeft: '5%', marginRight:'5%'}}>
           <NavigationBar />
-          <hr style={{marginLeft: '-12%', marginRight:'-12%'}}/>
+          <hr style={{marginLeft: '-2%', marginRight:'-2%', marginBottom:'-10%'}}/>
         </div>
-        <div>
-            <img src={require('./flogo.jpg')} alt="flogo" width="1550px" height="600px" />
-        </div>
+       <section id="main">
+                <div className="main-text" style={{ marginTop: "10%" }}>
+                    <span>Investing in your future is now </span> <br /> Smarter. <br />
+                    Simpler.<br /> Safer.<br/> 
+             </div>
+             <UndrawReport
+                    primaryColor='#7c7cdd'
+                    height='520px'
+             />
+        </section>
+        <Footer />
       </div>
     );
 }
 
 export default FrontPage;
+
